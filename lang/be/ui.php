deb6e55daad2876294db0c86cc5fd74dcb64b78a
Dzianis Kotau <me@dzianiskotau.com>
2026-04-05 19:44:00 +0200

    Translations

---
+++ lang/be/ui.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author Dzianis Kotau <me@dzianiskotau.com>
+ */
+
+return [
+    'block' => 'Ключы доступу',
+    'activate' => 'Актываваць',
+    'deactivate' => 'Дэактываваць',
+    'passkey-container' => [
+        'new-passkey-name-label' => 'Імя',
+        'new-passkey-name-placeholder' => 'Мой iPhone, Хатні ПК і г.д.',
+        'new-passkey-name-button' => 'Стварыць',
+        'table' => [
+            'headers' => [
+                'name' => 'Імя',
+                'updated-at' => 'Апошняе выкарыстанне',
+            ],
+        ],
+        'modal' => [
+            'title' => 'Пацверджанне',
+            'content' => 'Вы ўпэўнены, што жадаеце выдаліць гэты ключ доступу?',
+            'button' => 'Выдаліць',
+        ],
+    ],
+];
